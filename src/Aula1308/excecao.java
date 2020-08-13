dacb08f1589e8a669c6170a5538c915e84868e66
Stefanie-C <correastefanie10@gmail.com>
2020-08-13 19:49:12 -0300

    primeira versão

---
+++ src/Aula1308/excecao.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Aula1308;
+
+/**
+ *
+ * @author Stéfanie
+ */
+public class excecao {
+    public static void main(String[] args) {
+        System.out.println("CONSEGUI!");
+    }
+}
